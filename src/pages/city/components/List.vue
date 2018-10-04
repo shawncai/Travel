cf95ccfb9acaba9e40c454d26295778d30e9e982
shawnzhang <784836221@qq.com>
2018-10-04 14:33:45 +0800

    use vuex

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -6,7 +6,7 @@
 			<div class="button-list">
 				<div class="button-wrapper" >
 					<div class="button">
-					北京
+					{{this.currentCity}}
 					</div>
 				</div>
 			</div>
@@ -14,7 +14,8 @@
 		<div class="area">
 			<div class="title border-topbottom">热门城市</div>
 			<div class="button-list">
-				<div class="button-wrapper" v-for="item of hot" :key="item.id">
+				<div class="button-wrapper" v-for="item of hot" :key="item.id"
+				@click="handleCityClick(item.name)">
 					<div class="button">
 					{{item.name}}
 					</div>
@@ -29,6 +30,7 @@
 					
 					<div class="item border-bottom"
 					v-for="innerItem of item" :key="innerItem.id"
+					@click="handleCityClick(innerItem.name)"
 					>
 					{{innerItem.name}}
 					</div>
@@ -43,12 +45,24 @@
 
 <script>
  import Bscroll from 'better-scroll'
+ import { mapState, mapMutations } from 'vuex'
 	export default{
 		name: 'CityList',
 		props: {
 			hot: Array,
 			cities: Object,
 			letter: String
+		},
+		computed: {
+			...mapState({currentCity: 'city'})
+		},
+		methods: {
+			handleCityClick (city) {
+				
+				this.changeCity(city)
+				this.$router.push('/')
+			},
+			...mapMutations(['changeCity'])
 		},
 		mounted () {
 			this.scroll = new Bscroll(this.$refs.wrapper)
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -7,7 +7,7 @@
 	ref="search"
 	v-show="keyword">
 		<ul>
-			<li class="serach-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+			<li class="serach-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">{{item.name}}</li>
 			<li class="serach-item border-bottom" 
 			v-show="hasNoData">没有找到匹配数据</li>
 		</ul>
@@ -17,6 +17,7 @@
 
 <script>
 	import Bscroll from 'better-scroll'
+	import { mapMutations } from 'vuex'
 	export default{
 		name: 'CitySearch',
 		props: {
@@ -28,6 +29,14 @@
 				list: [],
 				timer: null
 			}
+		},
+		methods: {
+			handleCityClick (city) {
+				
+				this.changeCity(city)
+				this.$router.push('/')
+			},
+			...mapMutations(['changeCity'])
 		},
 		computed: {
 			hasNoData () {
